c1804651367292b652edcfb96c5a9a2c6be84953
Will Hunt <williamhunt1@gmail.com>
2017-05-21 22:38:56 +0800

    changed order of imports in app.js

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var db = require('./models/db');
+
 var index = require('./routes/index');
 var users = require('./routes/users');
-
-var db = require('./models/db');
 
 var app = express();
 
