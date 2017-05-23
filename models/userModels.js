9e3bf3aa13a2cdfd35277545659f7f7452ffc69c
Will Hunt <williamhunt1@gmail.com>
2017-05-23 14:23:16 +0800

    remove password field

--- models/userModels.js
+++ models/userModels.js
@@ -4,7 +4,6 @@ var passportLocalMongoose = require('passport-local-mongoose');
 var userSchema = new mongoose.Schema({
 	email:{type:String, required:true},
 	username:{type:String, required:true},
-	password:{type:String},
 	name:{type:String, required:true},
 	dateOfBirth:{type:String, required:true},
 	gender:{type:String, required:true},
