2c44b8be6ed4e0dfc3a1a2dd5e6706fa7d64f587
Will Hunt <williamhunt1@gmail.com>
2017-05-24 20:47:31 +0800

    finished discover page

--- public/js/discover.js
+++ public/js/discover.js
@@ -3,8 +3,10 @@ var pictures = ['1.jpg', '3.jpg', '4.jpg', '5.jpg'];
 var src = 'images/dogs/';
 var id = 'id_response';
 
-function discover(val) {
+function discover(val, form) {
+
     var responseElement = document.getElementById(id);
     responseElement.value = val;
+    form.submit();
 }
 
